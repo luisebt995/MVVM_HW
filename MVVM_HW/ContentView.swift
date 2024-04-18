c5760d866d5033bede94361872b750d44a2cd815
Luis Bustamante <luisebt995@gmail.com>
2024-04-18 15:38:34 +0200

    Initial Commit

---
+++ MVVM_HW/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MVVM_HW
+//
+//  Created by Luis on 18/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MVVM_HW/MVVM_HWApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_HWApp.swift
+//  MVVM_HW
+//
+//  Created by Luis on 18/4/24.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_HWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
